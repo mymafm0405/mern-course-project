d5925b920eece2275c2159f6f20908c85dd2e2b9
Mahmoud Sliman <mahmoud@Mahmouds-MBP.hotspot>
2022-10-04 06:06:25 +0300

    Link removed

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import Card from "../../shared/components/UIElements/Card";
 
 import "./PlaceItems.css";
